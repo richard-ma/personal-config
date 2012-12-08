b0c329652e0cbbd7d6b16bb01cc661d7c66d4208
Richard Ma <richard.ma.19850509@gmail.com>
2012-12-08 18:54:11 +0800

    add cc-mode

--- emacs/cstyle.el
+++ emacs/cstyle.el
@@ -1,12 +1,13 @@
 (provide 'cstyle)
 
-(defun my-c-mode-common-hook ()
-  (c-set-style "k&r")
-  (c-set_offset 'member-init-intro '++)
-  (setq default-tab-width 2)
-  (setq indent-tabs-mode nil)
-  (c-toggle-auto-hungry-state 1)
-  (font-lock-mode 2)
-  (define-key c-mode-base-map "\C-m" 'newline-and-indent))
-
-(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+;; cc-mode
+(add-to-list 'load-path "~/.emacs.d/plugins/cc-mode")
+(require 'cc-mode)
+(setq c-basic-offset 4) ;; 默认缩进为4
+(setq c-default-style '((c-mode . "k&r")    ;; c代码使用k&r格式
+                        (awk-mode . "awk")  ;; awk代码使用awk格式
+                        (other . "linux"))) ;; 其他使用linux kernel格式
+(setq-default c-electric-flag nil) ;; 关闭根据;和}刷新缩进的功能，新手建议关闭
+(defun my-make-CR-do-indent () ;; RET后自动计算缩进
+       (define-key c-mode-base-map "\C-m" 'c-context-line-break))
+(add-hook 'c-initialization-hook 'my-make-CR-do-indent)
