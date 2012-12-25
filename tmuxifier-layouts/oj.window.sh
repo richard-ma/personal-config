47bbd84aae01a70aadda7c9bc94cd649059c812b
Richard Ma <richard.ma.19850509@gmail.com>
2012-12-25 09:56:53 +0800

    add some layout to tmuxifier

--- tmuxifier-layouts/oj.window.sh
+++ tmuxifier-layouts/oj.window.sh
@@ -1,16 +1,15 @@
 # Set window root path. Default is `$session_root`.
 # Must be called before `new_window`.
-window_root "~/Projects/ghosTalk"
+window_root "~/Projects/oj"
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.
-new_window "ghostalk"
+new_window "oj"
 
 # Split window into panes.
 #split_v 20
 #split_h 50
-split_v 20
-split_h 40
+split_v 10
 
 # Set active pane.
 select_pane 0
@@ -19,8 +18,6 @@ select_pane 0
 #run_cmd "top"     # runs in active pane
 #run_cmd "date" 1  # runs in pane 1
 run_cmd "vim" 0         # runs in active pane
-run_cmd "rails c" 1     # runs in pane 1
-run_cmd "rails s" 2     # runs in pane 2
 
 # Paste text
 #send_keys "top"    # paste into active pane
