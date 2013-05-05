3ae0549c510e056c7cded25a3cbcc930ecc08b72
Richard Ma <richard.ma.19850509@gmail.com>
2013-05-05 10:16:46 +0800

    add init system script

---
+++ init_install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# update the system
+#sudo apt-get update
+#sudo apt-get dist-upgrade
+
+# vim editor
+#sudo apt-get install vim -y
+
+# zsh
+#sudo apt-get install zsh -y
+
+# build essential
+#sudo apt-get install build-essential -y
+
+# version control system
+#sudo apt-get install git subversion -y
+
+# LAMP
+#sudo apt-get install apache2 php5 libapache2-mod-php5 mysql-server libapache2-mod-auth-mysql php5-mysql phpmyadmin -y
+
+# Imagemagick
+#sudo apt-get install imagemagick -y
+
+# Python Pip
+#sudo apt-get install python-pip -y
+#sudo pip install docutils
+
+# change default sh (The Last One)
+#chsh -s /bin/zsh
+#echo "Please logout to make default shell options work!"
