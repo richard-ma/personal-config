151f19a92caef4fff6ee16770d5d82fc023e0b32
Richard Ma <richard.ma.19850509@gmail.com>
2013-05-06 17:58:44 +0800

    update apt-get install command format

--- init_install.sh
+++ init_install.sh
@@ -1,36 +1,30 @@
 #!/usr/bin/env bash
 
 # update the system
-#sudo apt-get update
-#sudo apt-get dist-upgrade
+sudo apt-get update && sudo apt-get dist-upgrade
 
-# vim editor
-#sudo apt-get install vim -y
-
-# zsh
-#sudo apt-get install zsh -y
-
-# build essential
-#sudo apt-get install build-essential -y
-
-# version control system
-#sudo apt-get install git subversion -y
-
-# compress tools
-#sudo apt-get install rar unrar -y
-
-# LAMP
-#sudo apt-get install apache2 php5 libapache2-mod-php5 mysql-server libapache2-mod-auth-mysql php5-mysql phpmyadmin -y
-
-# Imagemagick
-#sudo apt-get install imagemagick -y
+sudo apt-get -y install \
+    vim \
+    zsh \
+    build-essential \
+    fpc \
+    git \
+    subversion \
+    rar \
+    unrar \
+    imagemagick \
+    apache2 \
+    php5 \
+    libapache2-mod-php5 \
+    mysql-server \
+    libapache2-mod-auth-mysql \
+    php5-mysql \
+    phpmyadmin \
+    gnome-shell \
+    python-pip 
 
 # Python Pip
-#sudo apt-get install python-pip -y
 #sudo pip install docutils
-
-# Gnome-shell
-sudo apt-get install gnome-shell -y
 
 # change default sh (The Last One)
 #chsh -s /bin/zsh
