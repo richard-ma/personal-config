796072d018fb5d374c74c0c7e2fed7c72bcc51a5
Richard Ma <richard.ma.19850509@gmail.com>
2014-07-26 13:33:09 +0800

    add host-jumper tmuxifier window config

---
+++ tmuxifier-layouts/host-jumper.window.sh
@@ -0,0 +1,27 @@
+# Set window root path. Default is `$session_root`.
+# Must be called before `new_window`.
+window_root "~/Projects/host-jumper"
+
+# Create new window. If no argument is given, window name will be based on
+# layout file name.
+new_window "host-jumper"
+
+# Split window into panes.
+#split_v 20
+#split_h 50
+split_v 20
+split_h 40
+
+# Set active pane.
+select_pane 0
+
+# Run commands.
+#run_cmd "top"     # runs in active pane
+#run_cmd "date" 1  # runs in pane 1
+run_cmd "vim" 0         # runs in active pane
+run_cmd "bundle" 1     # runs in pane 1
+run_cmd "rails s" 2     # runs in pane 2
+
+# Paste text
+#send_keys "top"    # paste into active pane
+#send_keys "date" 1 # paste into active pane
