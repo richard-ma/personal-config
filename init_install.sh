dbef0d763831475941aacfde5ac9ebfb4ed8d030
Richard Ma <richard.ma.19850509@gmail.com>
2016-04-26 10:29:51 +0800

    update

--- init_install.sh
+++ init_install.sh
@@ -11,37 +11,25 @@ sudo apt-get -y install \
     zsh \
     openssh-server \
     build-essential \
-    fpc \
-    openjdk-7-jre-headless \
     git \
-    subversion \
     rar \
     unrar \
     tofrodos \
     curl \
     imagemagick \
-    apache2 \
-    php5 \
-    libapache2-mod-php5 \
-    mysql-server \
-    libapache2-mod-auth-mysql \
-    php5-mysql \
-    php5-curl \
-    php-pear \
     phpmyadmin \
     python-pip \
     python-vte \
     gimp \
-    miredo \
-    exuberant-ctags \
+    #miredo \
     p7zip \
-    google-chrome-stable
+    ibus-sunpinyin
 
 ## PPA
 # ffmpeg
-sudo add-apt-repository -y ppa:jon-severinsson/ffmpeg
-sudo apt-get -y update
-sudo apt-get -y install ffmpeg
+#sudo add-apt-repository -y ppa:jon-severinsson/ffmpeg
+#sudo apt-get -y update
+#sudo apt-get -y install ffmpeg
 
 # change default sh (The Last One)
 chsh -s /bin/zsh
